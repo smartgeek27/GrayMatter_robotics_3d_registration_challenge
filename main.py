e5476e573ce1d9e0532244ed3de413e919fae7ae
smartgeek69 <rish7a@gmail.com>
2022-03-22 11:38:34 -0700

    first commit

--- main.py
+++ main.py
@@ -4,19 +4,29 @@
 import numpy
 import time
 import pathlib
+
 ####
 
 #### TODO: ADD YOUR IMPORTS HERE
 from pypointmatcher import pointmatcher as pm, pointmatchersupport as pms
 ####
+
+ # TODO: Add your code here
+
+# saving pointmatcher library and datapoints to variables.
 PM = pm.PointMatcher
 DP = PM.DataPoints
+# parameters
 Parameters = pms.Parametrizable.Parameters
 
+# Defining registration model 
+
 def model(data,ref):
+    # extracting icp 
     icp = PM.ICP()
     params = Parameters()
-    # Prepare reading filters
+
+    # Prepare reading filters to remove noise
     name = "MinDistDataPointsFilter"
     params["minDist"] = "1.0"
     minDist_read = PM.get().DataPointsFilterRegistrar.create(name, params)
@@ -27,7 +37,7 @@ def model(data,ref):
     rand_read = PM.get().DataPointsFilterRegistrar.create(name, params)
     params.clear()
 
-    # Prepare reference filters
+    # Preparing reference filters to remove noise
     name = "MinDistDataPointsFilter"
     params["minDist"] = "1.0"
     minDist_ref = PM.get().DataPointsFilterRegistrar.create(name, params)
@@ -37,27 +47,32 @@ def model(data,ref):
     params["prob"] = "0.4"
     rand_ref = PM.get().DataPointsFilterRegistrar.create(name, params)
     params.clear()
+    
+    # defining matching algorithm
 
     name = "KDTreeMatcher"
     params["knn"] = "5"
     params["epsilon"] = "3"
     kdtree = PM.get().MatcherRegistrar.create(name, params)
     params.clear()
-
+ 
+    # trimming outliers
     name = "TrimmedDistOutlierFilter"
     params["ratio"] = "0.9"
     trim = PM.get().OutlierFilterRegistrar.create(name, params)
     params.clear()
 
+    # Error minimizing algorithm
     name = "PointToPointErrorMinimizer"
     pointToPoint = PM.get().ErrorMinimizerRegistrar.create(name)
     params.clear()
 
-
+    # Defining iteration to perform transformation checker
     name = "CounterTransformationChecker"
     params["maxIterationCount"] = "150"
     maxIter = PM.get().TransformationCheckerRegistrar.create(name, params)
     params.clear()
+
 
     name = "DifferentialTransformationChecker"
     params["minDiffRotErr"] = "0.001"
@@ -66,13 +81,15 @@ def model(data,ref):
     diff = PM.get().TransformationCheckerRegistrar.create(name, params)
     params.clear()
 
+    # inspecting NaN
     name = "NullInspector"
     nullInspect = PM.get().InspectorRegistrar.create(name)
 
     # Prepare transformation
     name = "RigidTransformation"
     rigid_trans = PM.get().TransformationRegistrar.create(name)
-
+ 
+    # applying all the defined params and filters in the model.
     icp.readingDataPointsFilters.append(minDist_read)
     icp.readingDataPointsFilters.append(rand_read)
     icp.referenceDataPointsFilters.append(minDist_ref)
@@ -84,30 +101,41 @@ def model(data,ref):
     icp.transformationCheckers.append(diff)
     icp.inspector = nullInspect
     icp.transformations.append(rigid_trans)
+    # computing transformation matrix
     T = icp(data, ref)
     return T
 
+###
+
+# main function 
 def main():
     data_list = ["bag", "basketball", "computercluster1", "corner2", "lab1", "sofalong", "sofawhole", "threechair","threemonitor"]
 
+    # setting path location as there is only one file. No need to connect to parent class.
     repo_location = pathlib.Path(__file__).parent
     data_folder = repo_location / 'data'
 
-
+    # creating dict to store error values
     Error={}
     for location in data_list:
+        # accessing the files one by one.
         path_to_ply1 = data_folder / location / 'kinect.ply'
         path_to_ply2 = data_folder / location / 'sfm.ply'
         path_to_Tgt = data_folder / location / 'T_gt.txt'
+        # timer
         start_time = time.time()
+        # providing path and storing in numpy for ref and data.
         T_gt = numpy.loadtxt("../"+str(path_to_Tgt))
-        # TODO: Add your code here
         data= DP(DP.load("../"+str(path_to_ply2)))
-        print("../"+str(path_to_ply2))
         ref=DP(DP.load(str("../"+str(path_to_ply1))))
+        # calling the model
         T = model(data,ref)
+        # calculating error values
         n=numpy.square(numpy.subtract(T, T_gt)).mean()
+        # storing error values and compt time
         Error[location]=n,(time.time() - start_time)
     print(Error)
+
+
 if __name__ == "__main__":
     main()
---
+++ readme.md
@@ -0,0 +1,22 @@
+The project aims to prepare the algorithm to register 2 point clouds of the similar object by means any registration algorithm or filters in libpointmatcher library. 
+The folder contains 3 files: 
+1. main.py -  Th working code file for the task, all the implementation can be seen in the To do section where the main() is called upon to load all the files of the test case and runs the model for point cloud registration.
+2. results.txt - The best results can be seen in the results.txt file for each point cloud class. A evaluation method to compare the error between transformation matrix an the ground truth is proposed as in this ex :
+
+'bag': (0.0015420208909381781, 0.5306024551391602).
+The prior value shows the error , the later shows the computation time to run the algorith.
+
+The error is computed by the means squared difference algorithm. The lower the value, more close the transformation matrix is to the ground truth. 
+Observation: As evident in the results file 5/10 test cases passed completely with error values even less than 0.1. Also rest of the error values are less than 1 except "threemonitor" and "threechair". It is because the scanned 'sfm' and 'kinect' files are fairly unordered and individual filters cannot be applied to modify the test case files.
+
+3. readme.txt - in it.
+4. data- test files.
+
+How to run:
+
+1. install required dependencies 
+2. Add path of file from data folder
+3. run the code
+4. Output - Error and computation time
+            Transformation matrix
+
